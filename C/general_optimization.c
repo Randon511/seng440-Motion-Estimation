a16a28dc953e8b069f19881cdda01b520cc28ee5
Randon <randon551@gmail.com>
2022-08-06 03:09:17 -0700

    Optimizing the diff calculation in calc_block_diff and doing some operator strength reduction

--- C/general_optimization.c
+++ C/general_optimization.c
@@ -24,7 +24,7 @@ static inline void readImage(char filename[], uint8_t pixels[HEIGHT][WIDTH])
     uint32_t bitsPerPixel;
     fseek(bmp, BITS_PER_PIXEL_OFFSET, SEEK_SET);
     fread(&bitsPerPixel, 2, 1, bmp);
-    uint32_t bytesPerPixel = bitsPerPixel / 8;
+    uint32_t bytesPerPixel = bitsPerPixel >> 3;
     uint32_t paddedRowSize = WIDTH * bytesPerPixel;
     // save pixel data to array
     uint32_t i,j;
@@ -40,17 +40,19 @@ static inline void readImage(char filename[], uint8_t pixels[HEIGHT][WIDTH])
     fclose(bmp);
 }
 
-static inline uint16_t calc_block_diff(uint16_t x_first_pixel, uint16_t y_first_pixel, 
-                                        uint16_t x_second_pixel, uint16_t y_second_pixel, 
+static inline uint16_t calc_block_diff(register uint16_t x_first_pixel, register uint16_t y_first_pixel, 
+                                        register  uint16_t x_second_pixel, register uint16_t y_second_pixel, 
                                         uint8_t image_first[HEIGHT][WIDTH], uint8_t image_second[HEIGHT][WIDTH])
-{
-    uint16_t SAD_temp = 0;
-    uint8_t x, y;
-    for (x = 0; x < 16; x++)
+{   
+    register uint16_t SAD_temp = 0;
+    register uint8_t x, y;
+    for (y = 0; y < 16; y++)
     {
-        for (y = 0; y < 16; y++)
+        uint8_t * img_second_y = *(image_second + y_second_pixel + y);
+        uint8_t * img_first_y = *(image_first + y_first_pixel + y);
+        for (x = 0; x < 16; x++)
         {
-            int16_t diff = image_second[y_second_pixel + y][x_second_pixel + x] - image_first[y_first_pixel + y][x_first_pixel + x];
+            int16_t diff = *(img_second_y + x_second_pixel + x) - *(img_first_y + x_first_pixel + x);
             if (diff < 0)
             {
                 SAD_temp -= diff;
@@ -72,26 +74,26 @@ int main(void)
     readImage("frame_1.bmp", image_first);
     readImage("frame_2.bmp", image_second);
     int8_t min_SAD_vals[15][20][2] = {};
-    uint8_t y_first, x_first;
+    register uint8_t y_first, x_first;
     // For each 16x16 block in first image
     for (x_first = 0; x_first < 20; x_first++)
     {
-        uint16_t x_first_pixel = x_first * 16;
+        register uint16_t x_first_pixel = x_first << 4;
         for (y_first = 0; y_first < 15; y_first++)
         { 
-            uint16_t y_first_pixel = y_first * 16;
+            register uint16_t y_first_pixel = y_first << 4;
             // Min SAD value for the current block
             uint16_t min_SAD = UINT16_T_MAX;
             // Block with the associated min SAD value
             uint8_t min_x, min_y = 0;
             // For each 16x16 block in second image
-            register uint8_t y_second, x_second;
+            uint8_t y_second, x_second;
             for (x_second = 0; x_second < 20; x_second++)
             {
-                uint16_t x_second_pixel = x_second * 16;
+                register uint16_t x_second_pixel = x_second << 4;
                 for (y_second = 0; y_second < 15; y_second++)
                 {
-                    uint16_t y_second_pixel = y_second * 16;
+                    uint16_t y_second_pixel = y_second << 4;
                     // Calculate SAD value for pair of blocks
                     uint16_t SAD_temp = calc_block_diff(x_first_pixel, y_first_pixel, 
                                                     x_second_pixel, y_second_pixel, 
