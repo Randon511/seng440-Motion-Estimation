9b292e776e0759333581748bb2cac0d3c7263859
Randon <randon551@gmail.com>
2022-08-06 23:50:38 -0700

    Undoing matrix access changes and fixing a bug with pixel variable size

--- C/cbd_opt.c
+++ C/cbd_opt.c
@@ -40,20 +40,18 @@ static inline void readImage(char filename[], uint8_t pixels[HEIGHT][WIDTH])
     fclose(bmp);
 }
 
-static inline uint16_t calc_block_diff(uint8_t x_first_pixel, uint8_t y_first_pixel, 
-                                        uint8_t x_second_pixel, uint8_t y_second_pixel, 
+static inline uint16_t calc_block_diff(uint16_t x_first_pixel, uint16_t y_first_pixel, 
+                                        uint16_t x_second_pixel, uint16_t y_second_pixel, 
                                         uint8_t image_first[HEIGHT][WIDTH], uint8_t image_second[HEIGHT][WIDTH])
 {   
     register uint16_t SAD_temp = 0;
     register uint8_t y;
+
     for (y = 0; y < 16; y++)
     {
-        uint8_t * img_second_y = *(image_second + y_second_pixel + y);
-        uint8_t * img_first_y = *(image_first + y_first_pixel + y);
-
-        uint8_t * base_first = img_first_y + x_first_pixel;
-        uint8_t * base_second = img_second_y + x_second_pixel;
-
+        uint8_t * base_first = &image_first[y_first_pixel + y][x_first_pixel];
+        uint8_t * base_second = &image_second[y_second_pixel + y][x_second_pixel];
+        
         SAD_temp += abs(*(base_second) - *(base_first)) +
                     abs(*(base_second + 1) - *(base_first + 1)) +
                     abs(*(base_second + 2) - *(base_first + 2)) +
@@ -74,22 +72,23 @@ static inline uint16_t calc_block_diff(uint8_t x_first_pixel, uint8_t y_first_pi
     return SAD_temp;
 }
 
+
 int main(void)
 {
     //clock_t start = clock();
     uint8_t image_first[HEIGHT][WIDTH];
     uint8_t image_second[HEIGHT][WIDTH];
     readImage("frame_1.bmp", image_first);
-    readImage("frame_2.bmp", image_second);
+    readImage("frame_1.bmp", image_second);
     int8_t min_SAD_vals[15][20][2] = {};
     register uint8_t y_first, x_first;
     // For each 16x16 block in first image
     for (x_first = 0; x_first < 20; x_first++)
     {
-        register uint8_t x_first_pixel = x_first << 4;
+        register uint16_t x_first_pixel = x_first << 4;
         for (y_first = 0; y_first < 15; y_first++)
         { 
-            register uint8_t y_first_pixel = y_first << 4;
+            register uint16_t y_first_pixel = y_first << 4;
             // Min SAD value for the current block
             uint16_t min_SAD = UINT16_T_MAX;
             // Block with the associated min SAD value
@@ -98,10 +97,10 @@ int main(void)
             uint8_t y_second, x_second;
             for (x_second = 0; x_second < 20; x_second++)
             {
-                register uint8_t x_second_pixel = x_second << 4;
+                register uint16_t x_second_pixel = x_second << 4;
                 for (y_second = 0; y_second < 15; y_second++)
                 {
-                    uint8_t y_second_pixel = y_second << 4;
+                    register uint16_t y_second_pixel = y_second << 4;
                     // Calculate SAD value for pair of blocks
                     uint16_t SAD_temp = calc_block_diff(x_first_pixel, y_first_pixel, 
                                                     x_second_pixel, y_second_pixel, 
